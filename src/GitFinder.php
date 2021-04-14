13518b8d1880729314161e2b17a876c62cb3391e
Albert Peschar <albert@peschar.net>
2021-04-14 08:25:50 +0300

    Use git ls-files -z

--- src/GitFinder.php
+++ src/GitFinder.php
@@ -14,9 +14,9 @@ class GitFinder implements IteratorAggregate {
     }
 
     public function getIterator() {
-        $proc = new Process(['git', 'ls-files', '*.php'], $this->root);
+        $proc = new Process(['git', 'ls-files', '-z', '*.php'], $this->root);
         $proc->start();
-        foreach ($this->getLines($proc->getIterator(Process::ITER_SKIP_ERR)) as $line) {
+        foreach ($this->getLines($proc->getIterator(Process::ITER_SKIP_ERR), "\x00") as $line) {
             yield new SplFileInfo(sprintf('%s/%s', $this->root, trim($line)));
         }
         if ($proc->wait() !== 0) {
@@ -24,11 +24,11 @@ class GitFinder implements IteratorAggregate {
         }
     }
 
-    private function getLines(\Generator $generator): \Generator {
+    private function getLines(\Generator $generator, string $delimiter): \Generator {
         $buffer = '';
         foreach ($generator as $chunk) {
             $buffer .= $chunk;
-            while (($pos = strpos($buffer, "\n")) !== false) {
+            while (($pos = strpos($buffer, $delimiter)) !== false) {
                 yield substr($buffer, 0, $pos + 1);
                 $buffer = substr($buffer, $pos + 1);
             }
